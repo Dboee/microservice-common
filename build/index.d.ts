af287eb32d4ac704754fd851355f3739b7f46da1
Dboee <Davidboee@gmail.com>
2023-02-15 14:34:26 +0100

    fixes names bug

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,10 @@
-interface RGBColor {
-    red: number;
-    green: number;
-    blue: number;
-}
-declare const color: RGBColor;
-export default color;
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/unauthorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+// errors
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
 export * from './errors/unauthorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
+
+// middlewares
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
